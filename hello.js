0fc0040dd5562c185ec1312a63ed1ba6509ae83a
TacoPartyPack <75919477+TacoPartyPack@users.noreply.github.com>
2020-12-13 09:17:57 -0500

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,3 +1,9 @@
-var name = prompt("What is your name?")
-console.log(name);
-console.log(name==="Bill");
+var name = prompt("Are you a Passenger (P) or a Driver (D)");
+if(name==="D")
+{
+  alert("Congrats, you are a Driver now");
+}
+else
+{
+    alert("Congrats, you are a Passenger now");
+}
