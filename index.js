a7361ec7faa405062a0c12e8a3508b82aa003a9a
Jklein64 <jklein.64@verizon.net>
2020-12-13 11:41:58 -0500

    Done with the first page

--- index.js
+++
@@ -1,9 +0,0 @@
-import "./css/app.scss"
-import {
-    MDCRipple
-} from '@material/ripple/index';
-
-const ripple = new MDCRipple(document.querySelector('.mdc-button'));
-
-
-console.log("hello world!")
